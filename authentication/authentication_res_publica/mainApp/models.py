01cdab9c5bf2030b38d0df720ef4e5bf4dbeb8d2
Burkov Alexey <rubicks_cube@mail.ru>
2022-12-14 22:04:51 +0300

    Create db model

--- authentication/authentication_res_publica/mainApp/models.py
+++ authentication/authentication_res_publica/mainApp/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
-# Create your models here.
+
+class Users(models.Model):
+    user_id = models.AutoField(primary_key=True)
+    user_role = models.ForeignKey('UserRoles', models.DO_NOTHING, db_column='user_role')
+    user_name = models.CharField(max_length=200)
+    user_login = models.CharField(max_length=200, blank=True, null=True)
+    user_pass = models.CharField(max_length=200)
+    user_photo_id = models.CharField(max_length=100, blank=True, null=True)
+
+    class Meta:
+        managed = False
+        db_table = 'users'
