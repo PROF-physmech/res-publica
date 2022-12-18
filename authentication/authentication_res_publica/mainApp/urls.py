90077bf4b75670610d0c51e0388306bbd0ce6f29
Burkov Alexey <rubicks_cube@mail.ru>
2022-12-18 17:52:28 +0300

    Add urlconf of main app

--- authentication/authentication_res_publica/mainApp/urls.py
+++ authentication/authentication_res_publica/mainApp/urls.py
@@ -1,7 +1,21 @@
-from django.urls import path
+from django.urls import path, include
 
 from .views import schema_view
 
 urlpatterns = [
     path('docs', schema_view.with_ui('swagger')),
+    path('login/', include([
+        path('admin', AdminLogin.as_view()),
+        # path('user', ),
+    ])),
+    # path('utilize/', include([
+    #     path('create/', include([
+    #         path('adminCredentials', ),
+    #         path('userQr', ),
+    #     ])),
+    #     path('reset/', include([
+    #         path('adminPassword', ),
+    #         path('userQr', ),
+    #     ])),
+    # ]))
 ]
