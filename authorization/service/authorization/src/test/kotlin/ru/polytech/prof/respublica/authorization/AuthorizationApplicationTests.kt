2a0a897367637974437d8196d57e9ade74907628
Burkov Alexey <rubicks_cube@mail.ru>
2023-01-08 01:44:24 +0300

    Add spring boot project and description file

---
+++ authorization/service/authorization/src/test/kotlin/ru/polytech/prof/respublica/authorization/AuthorizationApplicationTests.kt
@@ -0,0 +1,13 @@
+package ru.polytech.prof.respublica.authorization
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class AuthorizationApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
